8e1105077adb34a9baaf081bbebd766a1e74d281
Roman <RKushnir@users.noreply.github.com>
2022-04-05 19:23:00 +0200

    Implement API endpoint for drawing rectangles

--- server/lib/canvas_server_web/controllers/rectangle_controller.ex
+++ server/lib/canvas_server_web/controllers/rectangle_controller.ex
@@ -1,7 +1,19 @@
 defmodule CanvasServerWeb.RectangleController do
   use CanvasServerWeb, :controller
+  alias CanvasServer.Drawing
+  alias CanvasServer.Drawing.Rectangle
 
-  def create(conn, _params) do
-    json(conn, %{})
+  def create(conn, params) do
+    canvas = Drawing.get_canvas!(params["canvas_id"])
+
+    with {:ok, %Rectangle{}} <- Drawing.draw_rectangle(canvas, params) do
+      json(conn, %{})
+    else
+      {:error, changeset} ->
+        conn
+        |> put_status(:unprocessable_entity)
+        |> put_view(CanvasServerWeb.ChangesetView)
+        |> render("error.json", changeset: changeset)
+    end
   end
 end
--- server/test/canvas_server_web/controllers/rectangle_controller_test.exs
+++ server/test/canvas_server_web/controllers/rectangle_controller_test.exs
@@ -1,8 +1,68 @@
 defmodule CanvasServerWeb.RectangleControllerTest do
   use CanvasServerWeb.ConnCase
+  alias CanvasServer.Drawing
+  alias CanvasServer.Repo
 
   test "POST /rectangles", %{conn: conn} do
-    conn = post(conn, "/canvases/d41fcb72-291b-4033-8639-c59a06c2c0cb/rectangles")
+    %{id: canvas_id} = canvas = Drawing.create_canvas!()
+
+    conn =
+      post(conn, "/canvases/#{canvas_id}/rectangles", %{
+        offset_top: "1",
+        offset_left: "2",
+        height: "3",
+        width: "4",
+        fill_character: "*",
+        outline_character: "%"
+      })
+
     assert json_response(conn, 200)
+
+    canvas = Repo.preload(canvas, :rectangles)
+    assert length(canvas.rectangles) == 1
+
+    rectangle = hd(canvas.rectangles)
+    assert rectangle.offset_top == 1
+    assert rectangle.offset_left == 2
+    assert rectangle.height == 3
+    assert rectangle.width == 4
+    assert rectangle.fill_character == "*"
+    assert rectangle.outline_character == "%"
+  end
+
+  test "POST /rectangles returns error if canvas is not found", %{conn: conn} do
+    assert_error_sent 404, fn ->
+      post(conn, "/canvases/d41fcb72-291b-4033-8639-c59a06c2c0cb/rectangles", %{
+        offset_top: "1",
+        offset_left: "2",
+        height: "3",
+        width: "4",
+        fill_character: "*"
+      })
+    end
+  end
+
+  test "POST /rectangles returns errors if rectangle attributes are invalid", %{conn: conn} do
+    %{id: canvas_id} = canvas = Drawing.create_canvas!()
+
+    conn =
+      post(conn, "/canvases/#{canvas_id}/rectangles", %{
+        offset_top: "1",
+        offset_left: "-2",
+        height: "3",
+        width: "4"
+      })
+
+    response = json_response(conn, 422)
+
+    canvas = Repo.preload(canvas, :rectangles)
+    assert length(canvas.rectangles) == 0
+
+    assert %{
+             "errors" => %{
+               "fill_character" => ["Either fill_character or outline_character must be present"],
+               "offset_left" => ["must be greater than or equal to 0"]
+             }
+           } = response
   end
 end
