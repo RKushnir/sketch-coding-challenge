e42245390d4d01e8d07ee28127402237e12afb25
Roman <RKushnir@users.noreply.github.com>
2022-04-06 18:29:05 +0200

    Handle odd arguments in the create canvas command

--- client/lib/canvas_client/commands/create_canvas.ex
+++ client/lib/canvas_client/commands/create_canvas.ex
@@ -3,12 +3,21 @@ defmodule CanvasClient.Commands.CreateCanvas do
   Sends a create canvas request to the server.
   """
 
-  def run(_args) do
+  def run([]) do
     with {:ok, canvas} <- http_client().create_canvas() do
       IO.puts("Created a canvas with id #{canvas.id}.")
     else
       _error -> IO.puts("Failed to create a canvas.")
     end
+  end
+
+  def run(args) do
+    args_string = Enum.join(args, ", ")
+
+    IO.write("""
+    Received odd arguments: #{args_string}.
+    Please, use `canvas_client help` to see usage instructions.
+    """)
   end
 
   defp http_client do
