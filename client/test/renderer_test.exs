f24b95e29fc7893ea236dd2d790e2fd9e02c4221
Roman <RKushnir@users.noreply.github.com>
2022-04-13 20:23:40 +0200

    Add tests for the examples in the challenge description

--- client/test/renderer_test.exs
+++ client/test/renderer_test.exs
@@ -178,4 +178,83 @@ defmodule CanvasClient.RendererTest do
              "   XXXXX  \n" <>
              "          " == Renderer.to_string(canvas)
   end
+
+  test "test fixture 1" do
+    rectangle1 = %Rectangle{
+      offset_left: 3,
+      offset_top: 2,
+      width: 5,
+      height: 3,
+      fill_character: "X",
+      outline_character: "@"
+    }
+
+    rectangle2 = %Rectangle{
+      offset_left: 10,
+      offset_top: 3,
+      width: 14,
+      height: 6,
+      fill_character: "O",
+      outline_character: "X"
+    }
+
+    canvas =
+      Renderer.new_canvas(25, 10)
+      |> Renderer.render_rectangle(rectangle1)
+      |> Renderer.render_rectangle(rectangle2)
+
+    assert "                         \n" <>
+             "                         \n" <>
+             "   @@@@@                 \n" <>
+             "   @XXX@  XXXXXXXXXXXXXX \n" <>
+             "   @@@@@  XOOOOOOOOOOOOX \n" <>
+             "          XOOOOOOOOOOOOX \n" <>
+             "          XOOOOOOOOOOOOX \n" <>
+             "          XOOOOOOOOOOOOX \n" <>
+             "          XXXXXXXXXXXXXX \n" <>
+             "                         " == Renderer.to_string(canvas)
+  end
+
+  test "test fixture 2" do
+    rectangle1 = %Rectangle{
+      offset_left: 14,
+      offset_top: 0,
+      width: 7,
+      height: 6,
+      fill_character: "."
+    }
+
+    rectangle2 = %Rectangle{
+      offset_left: 0,
+      offset_top: 3,
+      width: 8,
+      height: 4,
+      outline_character: "O"
+    }
+
+    rectangle3 = %Rectangle{
+      offset_left: 5,
+      offset_top: 5,
+      width: 5,
+      height: 3,
+      fill_character: "X",
+      outline_character: "X"
+    }
+
+    canvas =
+      Renderer.new_canvas(25, 9)
+      |> Renderer.render_rectangle(rectangle1)
+      |> Renderer.render_rectangle(rectangle2)
+      |> Renderer.render_rectangle(rectangle3)
+
+    assert "              .......    \n" <>
+             "              .......    \n" <>
+             "              .......    \n" <>
+             "OOOOOOOO      .......    \n" <>
+             "O      O      .......    \n" <>
+             "O    XXXXX    .......    \n" <>
+             "OOOOOXXXXX               \n" <>
+             "     XXXXX               \n" <>
+             "                         " == Renderer.to_string(canvas)
+  end
 end
